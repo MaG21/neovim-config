cb4d2741d847416faad9f8537a5c535d81aaad07
Manuel <mag2@cbord.com>
2024-10-14 22:09:05 -0400

    Make Telescope aware of git repositories

--- lua/mag/keymaps.lua
+++ lua/mag/keymaps.lua
@@ -42,23 +42,30 @@ end
 
 local status, telescope_builtin = pcall(require, "telescope.builtin")
 if status then
-  local function is_git_repo()
+  -- Get git params if possible, if not return empty table
+  local function git_params()
 		vim.fn.system("git rev-parse --is-inside-work-tree")
 
-		return vim.v.shell_error == 0
-	end
-
-  -- If possible, use Telescope with the Git root as the search directory
-  vim.keymap.set('n', "<leader>p", function ()
     local params = {}
-    if is_git_repo() then
+    -- if shell error is 0 then it means git it present
+		if vim.v.shell_error == 0 then
       params = {
         prompt_title = "Files in Git Root",
         cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
       }
-    end
+		end
 
-    telescope_builtin.find_files(params)
+    return params
+	end
+
+  -- if we're on a repository, only consider the files tracked by
+  -- git. If not a git repo, then fallback to default behaviour.
+  vim.keymap.set('n', "<leader>p", function ()
+    telescope_builtin.find_files(git_params())
+  end)
+
+  vim.keymap.set("n", "<leader>s", function()
+    telescope_builtin.live_grep(git_params())
   end)
 
 end
@@ -67,7 +74,6 @@ keymap("n", "<Leader>c", "<cmd>noh<CR>", opts)
 keymap("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", opts) -- open file explorer
 keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<cr>", opts) -- autoformat with detected fixer (ie phpcsfixer)
 keymap("n", "<Leader>o", ":only<CR>", opts) -- Make the current buffer the only visible one if the screen is splitted
-keymap("n", "<leader>s", "<cmd>Telescope live_grep<cr>", opts)
 keymap("n", "<Leader>y", '"*yy', opts) -- copy to clipboard
 keymap("v", "<Leader>y", '"*yy', opts)
 
