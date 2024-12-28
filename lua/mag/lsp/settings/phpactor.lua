2f6b98e426f8cce5e40877d70762abbf2b5a405a
Manuel <mag2@cbord.com>
2024-12-28 19:59:08 -0400

    Format

--- lua/mag/lsp/settings/phpactor.lua
+++ lua/mag/lsp/settings/phpactor.lua
@@ -1,19 +1,19 @@
 return {
-  filetypes = { 'php' },
-  init_options = {
-    ["language_server_phpstan.enabled"] = true,
-    ['worse_reflection.stub_dir'] = '%project_root%/.ide_helper',
-  },
-  settings = {
-    phpactor = {
-      editor = {
-        formatOnPaste = true,
-        formatOnType = true
-      },
-      hover = {
-        documentation = true,
-        references = true,
-      },
-    },
-  },
+	filetypes = { "php" },
+	init_options = {
+		["language_server_phpstan.enabled"] = true,
+		["language_server_phpstan.level"] = "9",
+	},
+	settings = {
+		phpactor = {
+			editor = {
+				formatOnPaste = true,
+				formatOnType = true,
+			},
+			hover = {
+				documentation = true,
+				references = true,
+			},
+		},
+	},
 }
